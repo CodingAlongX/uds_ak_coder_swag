08cec20244030f9f4ff91bb9963e5a02e8e3a9d8
smjXpro <smjxpro@gmail.com>
2019-04-16 16:04:26 +0600

    ListView

--- app/src/main/java/pro/smjx/coderswag/controller/MainActivity.kt
+++ app/src/main/java/pro/smjx/coderswag/controller/MainActivity.kt
@@ -2,12 +2,26 @@ package pro.smjx.coderswag.controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import pro.smjx.coderswag.R
+import pro.smjx.coderswag.model.Category
+import pro.smjx.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_expandable_list_item_1,
+            DataService.categories
+        )
+
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/pro/smjx/coderswag/model/Category.kt
+++ app/src/main/java/pro/smjx/coderswag/model/Category.kt
@@ -1,3 +1,7 @@
 package pro.smjx.coderswag.model
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
