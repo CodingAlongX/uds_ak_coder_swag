2cc29f7e775afc612c575bc35d9c820ec34816e8
smjXpro <smjxpro@gmail.com>
2019-04-25 01:20:48 +0600

    ViewHolder

--- app/src/main/java/pro/smjx/coderswag/adapters/CategoryAdapter.kt
+++ app/src/main/java/pro/smjx/coderswag/adapters/CategoryAdapter.kt
@@ -17,22 +17,30 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
+        val holder: ViewHolder
 
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        if (convertView == null) {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
 
-        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+            holder = ViewHolder()
 
-        println("Heavy Computing")
+            holder.cayegoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder
+        } else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
 
         val category = categories[position]
 
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
 
-        categoryName.text = category.title
-        categoryImage.setImageResource(resourceId)
+        holder.categoryName?.text = category.title
+        holder.cayegoryImage?.setImageResource(resourceId)
 
-        println(resourceId)
 
         return categoryView
     }
@@ -47,5 +55,11 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getCount(): Int {
         return categories.count()
+    }
+
+
+    private class ViewHolder {
+        var cayegoryImage: ImageView? = null
+        var categoryName: TextView? = null
     }
 }
